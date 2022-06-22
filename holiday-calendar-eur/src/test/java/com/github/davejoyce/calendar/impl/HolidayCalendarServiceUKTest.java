6e1096a0821d288abf837034ae3230590829ce67
davejoyce-lab49 <david.joyce@lab49.com>
2022-06-22 00:00:59 -0400

    #30 Improved test coverage

--- holiday-calendar-eur/src/test/java/com/github/davejoyce/calendar/impl/HolidayCalendarServiceUKTest.java
+++ holiday-calendar-eur/src/test/java/com/github/davejoyce/calendar/impl/HolidayCalendarServiceUKTest.java
@@ -3,7 +3,13 @@ package com.github.davejoyce.calendar.impl;
 import com.github.davejoyce.calendar.HolidayCalendar;
 import com.github.davejoyce.calendar.HolidayCalendarFactory;
 import com.github.davejoyce.calendar.HolidayCalendarService;
+import com.github.davejoyce.calendar.HolidayDate;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
 
 import static org.testng.Assert.*;
 
@@ -26,6 +32,34 @@ public class HolidayCalendarServiceUKTest {
 
         HolidayCalendar ukCalendar = calendarService.getHolidayCalendar();
         assertTrue(ukCalendar.getHolidays().stream().anyMatch(holiday -> "Boxing Day".equals(holiday.getName())));
+    }
+
+    @Test
+    public void testDateRoll_BoxingDayCase() {
+        HolidayCalendarFactory factory = new HolidayCalendarFactory();
+        HolidayCalendar calendar = factory.create(CODE);
+        assertNotNull(calendar);
+
+        List<HolidayDate> ukHolidays2021 = calendar.calculate(2021);
+        Optional<HolidayDate> foundBoxingDay = ukHolidays2021.stream()
+                                                             .filter(hd -> "Boxing Day".equals(hd.getHoliday().getName()))
+                                                             .findFirst();
+        assertTrue(foundBoxingDay.isPresent());
+        assertEquals(foundBoxingDay.get().getDate(), LocalDate.of(2021, Month.DECEMBER, 28));
+    }
+
+    @Test
+    public void testDateRoll_DefaultCase() {
+        HolidayCalendarFactory factory = new HolidayCalendarFactory();
+        HolidayCalendar calendar = factory.create(CODE);
+        assertNotNull(calendar);
+
+        List<HolidayDate> ukHolidays2021 = calendar.calculate(2020);
+        Optional<HolidayDate> foundNewYearsDay = ukHolidays2021.stream()
+                                                               .filter(hd -> "New Year's Day".equals(hd.getHoliday().getName()))
+                                                               .findFirst();
+        assertTrue(foundNewYearsDay.isPresent());
+        assertEquals(foundNewYearsDay.get().getDate(), LocalDate.of(2020, Month.JANUARY, 1));
     }
 
 }
