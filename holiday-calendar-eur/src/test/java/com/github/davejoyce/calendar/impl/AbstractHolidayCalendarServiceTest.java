de1d6e2330c2bbd7e575a276c0c8d6a0b343d392
davejoyce-lab49 <david.joyce@lab49.com>
2022-08-07 15:10:44 -0400

    #18 Work on improving unit tests

--- holiday-calendar-eur/src/test/java/com/github/davejoyce/calendar/impl/AbstractHolidayCalendarServiceTest.java
+++ holiday-calendar-eur/src/test/java/com/github/davejoyce/calendar/impl/AbstractHolidayCalendarServiceTest.java
@@ -1,14 +1,16 @@
 package com.github.davejoyce.calendar.impl;
 
-import com.github.davejoyce.calendar.Holiday;
-import com.github.davejoyce.calendar.HolidayCalendar;
-import com.github.davejoyce.calendar.HolidayCalendarFactory;
+import com.github.davejoyce.calendar.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Iterator;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import static org.testng.Assert.*;
@@ -19,13 +21,19 @@ public abstract class AbstractHolidayCalendarServiceTest {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
     final String calendarCode;
 
+    HolidayCalendarFactory factory;
+
     AbstractHolidayCalendarServiceTest(String calendarCode) {
         this.calendarCode = calendarCode;
     }
 
+    @BeforeClass
+    public void setupFactory() {
+        this.factory = new HolidayCalendarFactory();
+    }
+
     @Test
     public void testHolidayCalendarFactoryCreate() {
-        final HolidayCalendarFactory factory = new HolidayCalendarFactory();
         final HolidayCalendar calendar = factory.create(calendarCode);
         assertNotNull(calendar);
         assertEquals(calendar.getCode(), calendarCode);
@@ -36,14 +44,28 @@ public abstract class AbstractHolidayCalendarServiceTest {
           dependsOnMethods = "testHolidayCalendarFactoryCreate")
     public void testGetHolidayCalendar(String expectedHolidayName) {
         final Predicate<Holiday> expectedHoliday = (holiday -> expectedHolidayName.equals(holiday.getName()));
-
-        final HolidayCalendarFactory factory = new HolidayCalendarFactory();
         final HolidayCalendar calendar = factory.create(calendarCode);
         assertTrue(calendar.getHolidays().stream().anyMatch(expectedHoliday));
         logger.debug("HolidayCalendar contains expected Holiday: {}", expectedHolidayName);
     }
 
+    @Test(dataProvider = "expectedHolidayOccurrences")
+    public void testDateRoll(int year, String holidayName, LocalDate expectedHolidayOccurrence) {
+        final HolidayCalendarService calendarService = factory.getService(calendarCode);
+        final HolidayCalendar calendar = calendarService.getHolidayCalendar();
+
+        final Optional<HolidayDate> holidayDate = calendar.calculate(year)
+                .stream()
+                .filter(hd -> holidayName.equals(hd.getHoliday().getName()))
+                .findFirst();
+        assertTrue(holidayDate.isPresent());
+        assertEquals(holidayDate.get().getDate(), expectedHolidayOccurrence);
+    }
+
     @DataProvider
     abstract Iterator<Object[]> expectedHolidayNames();
+
+    @DataProvider
+    abstract Iterator<Object[]> expectedHolidayOccurrences();
 
 }
--- holiday-calendar-eur/src/test/java/com/github/davejoyce/calendar/impl/HolidayCalendarServiceCATest.java
+++ holiday-calendar-eur/src/test/java/com/github/davejoyce/calendar/impl/HolidayCalendarServiceCATest.java
@@ -1,55 +1,40 @@
 package com.github.davejoyce.calendar.impl;
 
 import com.github.davejoyce.calendar.*;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Optional;
 
 import static org.testng.Assert.*;
 
-public class HolidayCalendarServiceCATest {
+public class HolidayCalendarServiceCATest extends AbstractHolidayCalendarServiceTest {
 
     static final String CODE = "CA";
 
-    @Test
-    public void testHolidayCalendarFactoryCreate() {
-        HolidayCalendarFactory factory = new HolidayCalendarFactory();
-        HolidayCalendar calendar = factory.create(CODE);
-        assertNotNull(calendar);
-        assertEquals(calendar.getCode(), CODE);
+    public HolidayCalendarServiceCATest() {
+        super(CODE);
     }
 
-    @Test
-    public void testGetHolidayCalendar() {
-        HolidayCalendarService calendarService = new HolidayCalendarServiceCA();
-        assertTrue(calendarService.isProvided(CODE));
-
-        HolidayCalendar caCalendar = calendarService.getHolidayCalendar();
-        assertTrue(caCalendar.getHolidays().stream().anyMatch(holiday -> "Victoria Day".equals(holiday.getName())));
+    @DataProvider
+    @Override
+    Iterator<Object[]> expectedHolidayNames() {
+        final Object[] familyDay = {"Family Day"};
+        final Object[] victoriaDay = {"Victoria Day"};
+        return Arrays.asList(familyDay, victoriaDay).listIterator();
     }
 
-    @Test(dependsOnMethods = "testGetHolidayCalendar")
-    public void testBoxingDayRoll() {
-        final HolidayCalendarService calendarService = new HolidayCalendarServiceCA();
-        final HolidayCalendar caCalendar = calendarService.getHolidayCalendar();
-
-        final Optional<HolidayDate> boxingDay21 = caCalendar.calculate(2021)
-                                                            .stream()
-                                                            .filter(hd -> "Boxing Day".equals(hd.getHoliday().getName()))
-                                                            .findFirst();
-        assertTrue(boxingDay21.isPresent());
-        assertEquals(boxingDay21.get().getDate(), LocalDate.of(2021, Month.DECEMBER, 28));
-        assertEquals(boxingDay21.get().getDate().getDayOfWeek(), DayOfWeek.TUESDAY);
-
-        final Optional<HolidayDate> boxingDay22 = caCalendar.calculate(2022)
-                                                            .stream()
-                                                            .filter(hd -> "Boxing Day".equals(hd.getHoliday().getName()))
-                                                            .findFirst();
-        assertTrue(boxingDay22.isPresent());
-        assertEquals(boxingDay22.get().getDate(), LocalDate.of(2022, Month.DECEMBER, 26));
+    @DataProvider
+    @Override
+    Iterator<Object[]> expectedHolidayOccurrences() {
+        final Object[] christmas20 = {2020, "Christmas Day", LocalDate.of(2020, Month.DECEMBER, 25)};
+        final Object[] christmas21 = {2021, "Christmas Day", LocalDate.of(2021, Month.DECEMBER, 25)};
+        return Arrays.asList(christmas20, christmas21).listIterator();
     }
 
 }
